f33ac947bc65b1e55d329f8b3af9b3ecc5e49922
Daniel Barbosa <daniel@fala.email>
2018-08-21 16:18:04 -0700

    tried bipedal walker

--- envs/bipedalwalker.py
+++ envs/bipedalwalker.py
@@ -4,7 +4,10 @@ from monitor import train, watch
 from dqn_agent import Agent
 from environment import Environment
 
+"""
+NOTE: need to make some modifications to support multiple actions
+"""
 
-environment = Environment('BipedalWalker-v2', 'gym', action_bins(5,5,5,5))
-agent = Agent(state_size=24, action_size=256, fc1_units=64, fc2_units=128, seed=0)
+environment = Environment('BipedalWalker-v2', 'gym', action_bins=(5,5,5,5))
+agent = Agent(state_size=24, action_size=256, fc1_units=64, fc2_units=64, seed=0)
 train(environment, agent, n_episodes=4000, max_t=1000)
