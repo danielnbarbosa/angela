f49e0bec7ae503927652fe3577b74ab7194bdc48
Daniel Barbosa <daniel@fala.email>
2018-08-23 22:27:27 -0700

    adjusting several hyperparams for more VB testing

--- envs/visualbanana.py
+++ envs/visualbanana.py
@@ -7,10 +7,10 @@ NOTE: download pre-built Unity VisualBannana.app from: https://s3-us-west-1.amaz
 environment = UnityMLEnvironment('VisualBanana.app', 'visual')
 #environment = UnityMLEnvironment('VisualBanana_Linux/Banana.x86_64', 'visual')
 
-model = ConvQNet(state_size=(3, 84, 84), action_size=4, seed=0)
+model = ConvQNet(state_size=(3, 42, 42), action_size=4, seed=0)
 
 agent = Agent(model, action_size=4,
-              use_double_dqn=False,
+              use_double_dqn=True,
               use_prioritized_experience_replay=False)
 
 # don't forget to reset epsilon when continuing training
@@ -18,4 +18,4 @@ agent = Agent(model, action_size=4,
 train(environment, agent, n_episodes=10000, solve_score=13.0,
       eps_start=1.0,
       eps_end=0.01,
-      eps_decay=0.999)
+      eps_decay=0.997)
--- libs/agent.py
+++ libs/agent.py
@@ -13,7 +13,7 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.95            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 2e-4               # learning rate
+LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
