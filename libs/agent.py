94ca35ffc80391be495009fb38e5d533f28f82fd
Daniel Barbosa <daniel@fala.email>
2018-08-29 05:52:55 -0700

    revert hyperparams

--- libs/agent.py
+++ libs/agent.py
@@ -11,9 +11,9 @@ import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.95            # discount factor
+GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 9e-4               # learning rate
+LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
