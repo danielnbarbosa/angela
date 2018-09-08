396e0c18cae4c25aae01dc7379f11975553cc50b
Daniel Barbosa <daniel@fala.email>
2018-09-07 18:39:30 -0700

    going back to known good hyperparameters to try again

--- envs/unity/visualbanana.py
+++ envs/unity/visualbanana.py
@@ -4,7 +4,7 @@ from imports import *
 NOTE: download pre-built Unity VisualBannana.app from: https://s3-us-west-1.amazonaws.com/udacity-drlnd/P1/Banana/
 """
 
-SEED=42
+SEED=0
 #SEED = random.randint(0, 2 ** 30)
 #print('SEED: {}'.format(SEED))
 
@@ -16,15 +16,15 @@ model = Simple3DConvQNet(state_size=(3, 4, 84, 84), action_size=4, seed=SEED)
 
 
 agent = DQNAgent(model, action_size=4, seed=SEED,
-                 buffer_size=20000,
-                 gamma=0.95,
-                 lr=9e-4,
-                 use_double_dqn=True,
+                 buffer_size=10000,
+                 gamma=0.99,
+                 lr=5e-4,
+                 use_double_dqn=False,
                  use_prioritized_experience_replay=False)
 
 # don't forget to reset epsilon when continuing training
 #load(model, 'latest.pth')
 train_dqn(environment, agent, n_episodes=10000, solve_score=13.0,
           eps_start=1.0,
-          eps_end=0.05,
+          eps_end=0.01,
           eps_decay=0.995)
