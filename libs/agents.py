1b8bdcff6ef90dbb5be704d71dec685a51271ed0
Daniel Barbosa <daniel@fala.email>
2018-09-18 14:34:56 -0700

    put agent modules in package

--- libs/agents.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, 'libs/agents')
-
-from dqn import DQN
-from hc import HillClimbing
-from pg import PolicyGradient
---
+++ libs/agents/__init__.py
@@ -0,0 +1,3 @@
+from libs.agents.dqn import DQN
+from libs.agents.hc import HillClimbing
+from libs.agents.pg import PolicyGradient
