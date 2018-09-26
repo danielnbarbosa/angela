6474a7d7a346b4c92c7d7ddb9f10728b92863133
Daniel Barbosa <daniel@fala.email>
2018-09-25 18:08:13 -0700

    add PongDeterministic-v4 env

--- libs/agents/pg/training.py
+++ libs/agents/pg/training.py
@@ -4,12 +4,14 @@ Training loop.
 
 #import glob
 #import os
+import random
 import torch
 import libs.statistics
 
 
-def train(environment, agent, n_episodes=10000, max_t=2000,
+def train(environment, agent, seed=0, n_episodes=10000, max_t=2000,
           gamma=0.99,
+          max_noop=0,
           render=False,
           solve_score=100000.0,
           graph_when_done=False):
@@ -19,13 +21,16 @@ def train(environment, agent, n_episodes=10000, max_t=2000,
     ======
         environment: environment object
         agent: agent object
+        seed (int): Random seed
         n_episodes (int): maximum number of training episodes
         max_t (int): maximum number of timesteps per episode
         gamma (float): discount rate
+        max_noop (int): maximum number of initial noops at start of episode
         render (bool): whether to render the agent
         solve_score (float): criteria for considering the environment solved
         graph_when_done (bool): whether to show matplotlib graphs of the training run
     """
+    random.seed(seed)
     stats = libs.statistics.PolicyGradientStats()
 
     # remove checkpoints from prior run
@@ -37,6 +42,12 @@ def train(environment, agent, n_episodes=10000, max_t=2000,
         saved_log_probs = []
         rewards = []
         state = environment.reset()
+        # take a random amount of noop actions before starting episode to inject stochasticity
+        for i in range(random.randint(0, max_noop)):
+            if render:  # optionally render agent
+                environment.render()
+            state, reward, done = environment.step(0)
+        # training loop
         for t in range(max_t):
             if render:  # optionally render agent
                 environment.render()
