635bf1c717286ffae3695fe2f552b984cb20bb4d
Daniel Barbosa <daniel@fala.email>
2018-10-09 12:02:38 -0700

    add multi-agent AWS config for reacher env

---
+++ cfg/reacher20_ddpg_aws.py
@@ -0,0 +1,24 @@
+agent_type='ddpg'
+env_class='UnityMLVectorMultiAgent'
+model_class='LowDim2x'
+
+environment = {
+    'name': 'cfg/compiled_unity_environments/Reacher_Linux/Reacher.x86_64'
+}
+
+model = {
+    'state_size': 33,
+    'action_size': 4,
+}
+
+agent = {
+    'action_size': 4,
+    'update_every': 2,
+    'n_agents': 20,
+    'batch_size': 128,
+}
+
+train = {
+    'n_episodes': 1000,
+    'solve_score': 30.0,
+}
