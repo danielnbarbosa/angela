ad8fc200ec62af603779271c068100b37f97f7ec
Daniel Barbosa <daniel@fala.email>
2018-10-17 14:31:50 -0700

    add new pong_pg config

---
+++ cfg/pong/pong_pg_03.py
@@ -0,0 +1,27 @@
+algorithm='pg'
+env_class='GymAtariPong'
+model_class='TwoLayerConv2D'
+
+environment = {
+    'name': 'Pong-v4',
+}
+
+model = {
+    'state_size': (4, 80, 80),
+    'action_size': 3,
+    'filter_maps': (16, 32),
+    'kernels': (8, 4),
+    'strides': (4, 3),
+    'conv_out': 6,
+    'fc_units': 200
+}
+
+agent = {
+    'lr': 0.0001,
+    'action_map': {0: 0, 1: 4, 2: 5}
+}
+
+train = {
+    'n_episodes': 50000,
+    'max_t': 400,
+}
